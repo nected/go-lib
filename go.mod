7b2c75106a82a85532ef4a53530b75967b4ddcaf
Mrigesh Pokhrel <mrigesh@nected.ai>
2024-04-11 15:18:05 +0530

    zap logger integration ready
    Signed-off-by: Mrigesh Pokhrel <mrigesh@nected.ai>

--- go.mod
+++ go.mod
@@ -2,13 +2,17 @@ module github.com/nected/go-lib
 
 go 1.22.0
 
-require go.uber.org/zap v1.21.0
+require (
+	github.com/TheZeroSlave/zapsentry v1.20.2
+	github.com/getsentry/sentry-go v0.27.0
+	go.uber.org/zap v1.27.0
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
-	go.uber.org/atomic v1.9.0 // indirect
-	go.uber.org/multierr v1.9.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
--- logger/logger.go
+++ logger/logger.go
@@ -3,7 +3,10 @@ package logger
 import (
 	"fmt"
 
+	"github.com/TheZeroSlave/zapsentry"
+	"github.com/getsentry/sentry-go"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 type Logger struct {
@@ -13,12 +16,17 @@ type Logger struct {
 	logSentry     bool
 }
 
-func NewLogger() *Logger {
-	options := []zap.Option{
+func getOptions() []zap.Option {
+	return []zap.Option{
 		zap.AddCaller(),
-		zap.AddCallerSkip(5),
+		zap.AddCallerSkip(2),
+		zap.AddStacktrace(zap.ErrorLevel),
 	}
-	l, err := zap.NewProduction(options...)
+}
+
+func NewLogger() *Logger {
+	config := zap.NewProductionConfig()
+	l, err := config.Build(getOptions()...)
 	if err != nil {
 		fmt.Printf("failed to initialize zap logger: %v", err)
 	}
@@ -26,43 +34,120 @@ func NewLogger() *Logger {
 }
 
 func NewNamedLogger(name string) *Logger {
-	l, err := zap.NewProduction()
+	l, err := zap.NewProduction(getOptions()...)
 	if err != nil {
 		fmt.Printf("failed to initialize zap logger: %v", err)
 	}
 	return &Logger{log: l.Named(name)}
 }
 
+func (l *Logger) WithEnv(env string) *Logger {
+	var err error
+	switch env {
+	case "development", "dev":
+		l.log, err = zap.NewDevelopment(getOptions()...)
+		if err != nil {
+			fmt.Printf("failed to initialize zap logger: %v", err)
+		}
+	case "local":
+		l.log, err = zap.NewDevelopment(getOptions()...)
+		if err != nil {
+			fmt.Printf("failed to initialize zap logger: %v", err)
+		}
+	}
+	l.log = l.log.With(zap.String("env", env))
+	return l
+}
+
+func (l *Logger) AddCaller() *Logger {
+	l.log = l.log.WithOptions(zap.AddCaller())
+	return l
+}
+
+func (l *Logger) AddCallerSkip(skip int) *Logger {
+	l.log = l.log.WithOptions(zap.AddCallerSkip(skip))
+	return l
+}
+
+func (l *Logger) WithSentry(client *sentry.Client) *Logger {
+	l.sentryEnabled = true
+	l.logSentry = true
+	l.log = modifyToSentryLogger(l.log, client)
+	return l
+}
+
 // info log
-func (l *Logger) Info(code string, msg string, fields ...zap.Field) {
-	l.log.Info(msg, getZapFields(code, fields...)...)
+func (l *Logger) Info(msg string, args ...interface{}) {
+	l.log.With(getZapFields(args...)...).Info(msg)
 }
 
 // error log
-func (l *Logger) Error(code string, msg string, fields ...zap.Field) {
-	l.log.Error(msg, getZapFields(code, fields...)...)
+func (l *Logger) Error(msg string, args ...interface{}) {
+	l.log.With(getZapFields(args...)...).Error(msg)
 }
 
 // warn log
-func (l *Logger) Warn(code string, msg string, fields ...zap.Field) {
-	l.log.Warn(msg, getZapFields(code, fields...)...)
+func (l *Logger) Warn(msg string, args ...interface{}) {
+	l.log.With(getZapFields(args...)...).Warn(msg)
 }
 
 // debug log
-func (l *Logger) Debug(code string, msg string, fields ...zap.Field) {
-	l.log.Debug(msg, getZapFields(code, fields...)...)
+func (l *Logger) Debug(msg string, args ...interface{}) {
+	l.log.With(getZapFields(args...)...).Debug(msg)
 }
 
 // fatal log
-func (l *Logger) Fatal(code string, msg string, fields ...zap.Field) {
-	l.log.Fatal(msg, getZapFields(code, fields...)...)
+func (l *Logger) Fatal(msg string, args ...interface{}) {
+	l.log.With(getZapFields(args...)...).Fatal(msg)
 }
 
 // panic log
-func (l *Logger) Panic(code string, msg string, fields ...zap.Field) {
-	l.log.Panic(msg, getZapFields(code, fields...)...)
+func (l *Logger) Panic(msg string, args ...interface{}) {
+	l.log.With(getZapFields(args...)...).Panic(msg)
 }
 
-func getZapFields(code string, fields ...zap.Field) []zap.Field {
-	return append(fields, zap.String("code", code))
+func getZapFields(args ...interface{}) (fields []zapcore.Field) {
+	var fError error
+	fields = make([]zap.Field, 0)
+	errors := make([]error, 0)
+	for i := 0; i < len(args); i += 2 {
+		if i+1 >= len(args) {
+			break
+		}
+		key := args[i]
+		value := args[i+1]
+		// check if value is of type error
+		if err, ok := value.(error); ok {
+			errors = append(errors, err)
+			continue
+		}
+		fields = append(fields, zap.Any(key.(string), value))
+	}
+
+	if len(fields)*2 < len(args) {
+		// check if value is of type error
+		if err, ok := args[len(args)-1].(error); ok {
+			fError = err
+		}
+	}
+	fields = append(fields, zap.Errors("errors", errors), zap.Error(fError))
+	return fields
+}
+
+func modifyToSentryLogger(log *zap.Logger, client *sentry.Client) *zap.Logger {
+	cfg := zapsentry.Configuration{
+		Level:             zapcore.ErrorLevel, // when to send message to sentry
+		EnableBreadcrumbs: true,               // enable sending breadcrumbs to Sentry
+		BreadcrumbLevel:   zapcore.InfoLevel,  // at what level should we sent breadcrumbs to sentry, this level can't be higher than `Level`
+	}
+	core, err := zapsentry.NewCore(cfg, zapsentry.NewSentryClientFromClient(client))
+	// don't use value if error was returned. Noop core will be replaced to nil soon.
+	if err != nil {
+		panic(err)
+	}
+
+	log = zapsentry.AttachCoreToLogger(core, log)
+
+	// if you have web service, create a new scope somewhere in middleware to have valid breadcrumbs.
+	return log.With(zapsentry.NewScope())
 }
